6e04a2f108defd297b4e4296c6dd68ad46c0fda6
ej <elijah.cajucom@gmail.com>
2024-06-15 13:35:25 +0800

    psuhing changes

--- src/App.js
+++ src/App.js
@@ -1,79 +1,61 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink, useLocation, Navigate } from 'react-router-dom';
 import PrivateRoute from './utils/PrivateRoute';
 
 function App() {
+
   const [loggedIn, setLoggedIn] = useState(false)
-  const [component, setComponent] = useState(1)
   const [username, setUsername] = useState('') // to be used for homepage
 
   // when user logs in, set the following states
   const loginFunc = (number, user) => {
-    setComponent(number)
     setLoggedIn(true)
     setUsername(user)
   }
 
+  const urlName = useLocation().pathname
+  console.log(urlName)
+  console.log(loggedIn)
+
   // state tracking function to render compponent based on respective number (based on UL element for component to number match)
-  const handleComponent = (num) => {
-    if (num === 1) {
-      setComponent(1)
-      setLoggedIn(false)
-    } else if (num === 2){
-      setComponent(2)
-    } else if (num === 3) {
-      setComponent(3)
-    } else if (num === 4) {
-      setComponent(4)
-    }
-    
-  }
+
 
   return (
     <div className="bg-gray-200 h-screen">
       <header className={loggedIn ? 'w-full h-[70px] bg-gray-400' : 'hidden'}>
         {
-          component === 1 ? null :
-          <ul className='flex items-center justify-end gap-4 mx-4 h-full text-4xl font-thin'>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
-            <NavLink to='/contact'>Contact</NavLink>
-            <NavLink to='/login'>Logout</NavLink>
-            
-          </ul>
+          urlName !== '/login' && loggedIn === true ? 
+          null : 
+           <ul className='flex items-center justify-end gap-4 mx-4 h-full text-4xl font-thin'>
+           <NavLink to='/'>Home</NavLink>
+           <NavLink to='/about'>About</NavLink>
+           <NavLink to='/contact'>Contact</NavLink>
+           <NavLink to='/auth/login'>Logout</NavLink>
+           
+         </ul>
+          
         }
       </header>
-      {/* conditional rendering of components */}
-      {/* <div className='p-10'>
-        {!loggedIn && component === 1 ? <Login loginFunc={loginFunc}/> : null}
-        {component === 2 && <Home username={username}/>}
-        {component === 3 && <About />}
-        {component === 4 && <Contact/>}
-      </div> */}
 
       <div className='p-10'>
-        <BrowserRouter >
-
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
           <Routes>
             {/* elements inside can only be accessed if user is logged in  */}
             <Route path='/' element={<PrivateRoute loggedIn={loggedIn}/>}>
-              <Route path='/' index element={<Home username={username}/>}/>
-              <Route path='/about' element={<About/>}/>
-              <Route path='/contact' element={<Contact/>}/>
+              <Route index element={<Navigate to='home' replace={true}/>}/>
+              <Route path='home' element={<Home username={username}/>} />
+              <Route path='about' element={<About/>}/>
+              <Route path='contact' element={<Contact/>}/>
             </Route>
             
-            <Route path='/login' element={<Login loginFunc={loginFunc}/>}/>
+            <Route path='/auth/login' element={<Login loginFunc={loginFunc}/>}/>
           
           </Routes>
-        </BrowserRouter>
+        
       </div>
       
     </div>
--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,14 +1,13 @@
 import { Outlet, Navigate } from 'react-router-dom'
-import { useContext } from 'react'
 
 
 const PrivateRoute = ({ loggedIn }) => {
 
-    let user = loggedIn
+    const user = loggedIn
 
     // If user is authorized (basically succesfully logged in), return an outlet that will render child elements
     // If not, return element that will navigate to login page
-    return user ? <Outlet /> : <Navigate to='/login'/>
+    return user ? <Outlet /> : <Navigate to='/auth/login'/>
  
 }
 
